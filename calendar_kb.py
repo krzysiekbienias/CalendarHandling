171940e57bb332a9aece182fbd29701fc4016d06
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2018-10-17 21:24:25 +0200

    add new class "scenarios Calendar"

--- calendar_kb.py
+++ calendar_kb.py
@@ -3,12 +3,13 @@ import pandas as pd
 import bdateutil as bd
 import datetime as dt
 import calendar
+from pandas.tseries.offsets import *
 
-class Calendar_kb:
-    def __init__(self, today, maturity_date, day_convention):
+class CashflowsCalendar:
+    def __init__(self, today, day_convention,maturity_date=None):
         self.today = today
-        self.maturity_date = maturity_date
         self.day_convention = day_convention
+        self.maturity_date = maturity_date
 
     def get_time_step_fun(self, data_frame):
         return data_frame['Data_maturity']  # it depends from column names in excel that contains maturity data.
@@ -21,15 +22,16 @@ class Calendar_kb:
                 delta = (self.maturity_date.toordinal()-self.today.toordinal())/360
         if self.day_convention == '30/360':
                 delta = (max(30-self.today.day, 0)+min(self.maturity_date.day, 30)+360*(self.maturity_date.year-
-                Calendar_kb.today.year)+30*(self.maturity_date.month-self.month-1))/360
+                CashflowsCalendar.today.year)+30*(self.maturity_date.month-self.month-1))/360
         return delta
 
     def days_difference_fun(self):  # ONLY TWO CONVENTIONS
         if self.day_convention == 'Actual/365':
-            delta = (self.maturity_date.toordinal() - self.today.toordinal())
+            self.delta = (self.maturity_date.toordinal() - self.today.toordinal())
         if self.day_convention == 'Actual/360':
-            delta = (self.maturity_date.toordinal() - self.today.toordinal())
-        return delta
+            self.delta = (self.maturity_date.toordinal() - self.today.toordinal())
+
+        return self
 
     def year_fraction_fun(self, shift, period):
         if period == 'days':
@@ -67,10 +69,9 @@ class Calendar_kb:
         if period == 'days':
             next_dates = self.today + bd.relativedelta(days=+shift)
             check = calendar.day_name[next_dates.weekday()]
-            if check == 'Sunday':
-                next_dates = next_dates + bd.relativedelta(days=+1)
-            if check == 'Saturday':
-                next_dates = next_dates + bd.relativedelta(days=+2)
+            if check == 'Friday':
+                next_dates = next_dates + bd.relativedelta(days=+3)
+
         if period == 'months':
             next_dates = self.today + bd.relativedelta(months=+shift)
             check = calendar.day_name[next_dates.weekday()]
@@ -94,7 +95,8 @@ class Calendar_kb:
             if check == 'Sunday':
                 next_dates = next_dates + bd.relativedelta(days=+1)
             if check == 'Saturday':
-                next_dates = next_dates + bd.relativedelta(days=+2)
+                next_dates = next_dates + bd.relativedelta(days=+2)#TODO tak odliczanie od piatku tez nie dziala trzeba zrobic jak w mainie w przypadku days
+
         if period == 'months':
             next_dates = self.today + bd.relativedelta(months=+shift)
             check = calendar.day_name[next_dates.weekday()]
@@ -143,6 +145,46 @@ class Calendar_kb:
 
     def split_year_equal(self,parts):
         return int(round(365/parts,0))
+
+
+class ScenariosCalendar:
+    def __init__(self, today, day_convention,lag, maturity_date=None,live_of_contract=None):
+        self._stoday=today #only convention that this variable is private,_sxxx=string,sexxx=series
+        self._sday_convention=day_convention
+        self._smaturity_date=maturity_date
+        self._lag=lag
+        self.live_of_contract=live_of_contract
+        self.spot_date=self.get_spot_date()
+        self.se_date_series=self.get_date_series()
+        self.l_increments=self.get_increments()
+
+    def get_spot_date(self):#TODO if days=0 spot_date=today, add that
+        if (self._lag!=0):
+            spot_date=self._stoday+bd.relativedelta(days=+self._lag)
+        else:
+            spot_date = self._stoday
+        return spot_date
+
+    def year_fraction_two_dates_fun(self):
+        if self.day_convention == 'Actual/365':
+                delta = (self.maturity_date.toordinal()-self.spot_date.toordinal())/365
+        if self.day_convention == 'Actual/360':
+                delta = (self.maturity_date.toordinal()-self.today.toordinal())/360
+        if self.day_convention == '30/360':
+                delta = (max(30-self.today.day, 0)+min(self.maturity_date.day, 30)+360*(self.maturity_date.year-
+                CashflowsCalendar.today.year)+30*(self.maturity_date.month-self.month-1))/360
+        return delta
+
+    def get_date_series(self):
+        dates = pd.date_range(self.spot_date,self._smaturity_date, freq='B')
+        se_dates = pd.Series(dates)
+        return se_dates
+
+    def get_increments(self):
+        time_index=pd.date_range(self.spot_date,self._smaturity_date, freq='B')#time_index object
+        two_consequitive=[(time_index[i]-time_index[i-1]) for i in range(1,len(time_index))]
+        dt=[two_consequitive[i].days/365 for i in range(len(two_consequitive))]
+        return dt
 
 
 
--- main_calendar.py
+++ main_calendar.py
@@ -1,15 +1,12 @@
 import numpy as np
 import pandas as pd
+from pandas.tseries.offsets import *
 import bdateutil as bd
 import datetime as dt
 import calendar
-from calendar_kb import Calendar_kb
+from calendar_kb import CashflowsCalendar,ScenariosCalendar
 
 if __name__ == '__main__':
-    o_test_calendar=Calendar_kb(today=dt.date(2018,6,25),
-                                maturity_date=(2018,9,25),day_convention='Actual/365')
-
-    v_dates=o_test_calendar.move_dates_fun(shift=5,period='days')
-    splited=o_test_calendar.split_year_equal(27)
+    o_scenario_calendar=ScenariosCalendar(today='2018-08-20',day_convention='Actual/365',maturity_date='2018-11-20')
 
     print('The end of the programm')
